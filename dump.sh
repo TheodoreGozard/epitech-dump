c9713e8e5e692b56ed56868c0a6c2962632f080d
Extauren <extauren@protonmail.com>
2025-09-12 00:56:15 +0200

    Fix switch to offical epitech-emacs repo

--- dump.sh
+++ dump.sh
@@ -9,9 +9,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 packages_list=(
-    build-essential
-    clang-20
-    clang-tools
     epiclang
     banana-coding-style-checker
     emacs-nox
@@ -33,15 +30,13 @@ packages_list=(
     x264
     tree
     unzip
-    curl
     net-tools
     tcsh
-    sudo
     git
 )
 
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/llvm.asc
-echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/llvm.asc] https://apt.llvm.org/noble/ llvm-toolchain-noble-20 main" | sudo tee /etc/apt/sources.list.d/llvm.list
+# wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/llvm.asc
+# echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/llvm.asc] https://apt.llvm.org/noble/ llvm-toolchain-noble-20 main" | sudo tee /etc/apt/sources.list.d/llvm.list
 
 wget -O - https://epitech.github.io/epiclang/key.asc | sudo tee /etc/apt/trusted.gpg.d/epiclang-apt.asc
 echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/epiclang-apt.asc] https://epitech.github.io/epiclang/ testing main" | sudo tee /etc/apt/sources.list.d/epiclang.list
@@ -51,18 +46,23 @@ echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/banana-apt.asc] https://e
 
 apt update
 
+apt install -y build-essental curl
+
 apt install -y ${packages_list[@]}
 
-curl -sSL "https://github.com/Snaipe/Criterion/releases/download/v2.4.2/criterion-2.4.2-linux-x86_64.tar.xz" -o criterion-2.4.2.tar.xz
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-20 100 && update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-20 100
+
+curl -sSL "https://github.com/Snaipe/Ciriterion/releases/download/v2.4.2/criterion-2.4.2-linux-x86_64.tar.xz" -o criterion-2.4.2.tar.xz
 tar xf criterion-2.4.2.tar.xz
 cp -r criterion-2.4.2/* /usr/local/
 echo "/usr/local/lib" > /etc/ld.so.conf.d/usr-local.conf
 ldconfig
 rm -rf criterion-2.4.2.tar.xz criterion-2.4.2/
 
-git clone https://github.com/TheodoreGozard/epitech-emacs.git epitech-emacs
-sudo -u "#1000" ./epitech-emacs/INSTALL.sh local
-rm -rf epitech-emacs
+git clone https://github.com/Epitech/epitech-emacs.git
+cd epitech-emacs
+sudo -u "#1000" ./INSTALL.sh local
+cd .. && rm -rf epitech-emacs
 
 git clone https://github.com/Epitech/vim-epitech.git epitech-vim
 cd epitech-vim
