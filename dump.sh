d3da5ab810a4f613654876b39e2ca55b327f7dc9
Extauren <extauren@protonmail.com>
2025-10-02 18:14:58 +0200

    Add epitech ppa and install epitech-cpooll epitech-emacs and epitech-vim

--- dump.sh
+++ dump.sh
@@ -10,68 +10,25 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-packages_list=(
-    epiclang
-    banana-coding-style-checker
-    emacs-nox
-    gcovr
-    libcsfml-dev
-    libcsfml-doc
-    libjpeg-dev
-    libx11-xcb-dev
-    libx11-doc
-    libfreetype-dev
-    libvorbis-dev
-    libopenal-dev
-    libxi-dev
-    libxrandr-dev
-    libxcb-image0-dev
-    libxcb-util-dev
-    valgrind
-    vim
-    x264
-    tree
-    unzip
-    net-tools
-    tcsh
-    git
-)
-
 export DEBIAN_FRONTEND=noninteractive
+
+add-apt-repository -y -s ppa:epitech/ppa
+add-apt-repository -y -s universe
+apt update
+
+apt install -y wget
 
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/llvm.asc
 echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/llvm.asc] https://apt.llvm.org/noble/ llvm-toolchain-noble-20 main" | sudo tee /etc/apt/sources.list.d/llvm.list
-
-wget -O - https://epitech.github.io/epiclang/key.asc | sudo tee /etc/apt/trusted.gpg.d/epiclang-apt.asc
-echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/epiclang-apt.asc] https://epitech.github.io/epiclang/ testing main" | sudo tee /etc/apt/sources.list.d/epiclang.list
-
-wget -O - https://epitech.github.io/banana-next/key.asc | sudo tee /etc/apt/trusted.gpg.d/banana-apt.asc
-echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/banana-apt.asc] https://epitech.github.io/banana-next/ testing main" | sudo tee /etc/apt/sources.list.d/banana.list
-
 apt update
-
-apt install -y build-essential curl libllvm20
-
-apt install -y ${packages_list[@]}
-
+apt install -y clang-20
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-20 100 && update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-20 100
 
-curl -sSL "https://github.com/Snaipe/Ciriterion/releases/download/v2.4.2/criterion-2.4.2-linux-x86_64.tar.xz" -o criterion-2.4.2.tar.xz
-tar xf criterion-2.4.2.tar.xz
-cp -r criterion-2.4.2/* /usr/local/
-echo "/usr/local/lib" > /etc/ld.so.conf.d/usr-local.conf
-ldconfig
-rm -rf criterion-2.4.2.tar.xz criterion-2.4.2/
+apt install -y epitech-cpool
+apt install -y epitech-emacs
+apt install -y epitech-vim
 
-git clone https://github.com/Epitech/epitech-emacs.git
-cd epitech-emacs
-bash INSTALL.sh system
-cd .. && rm -rf epitech-emacs
-
-git clone https://github.com/Epitech/vim-epitech.git epitech-vim
-cd epitech-vim
-bash install.sh system
-cd .. && rm -rf epitech-vim
+apt purge -y postfix
 
 snap install teams-for-linux
 
