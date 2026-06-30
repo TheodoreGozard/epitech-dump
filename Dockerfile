4dd4e636453997f63f11bd90df2833cdaa5ef0de
Extauren <extauren@protonmail.com>
2026-06-30 15:46:34 +0200

    fix(stumper): remove old file move to alpine package repo

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,20 @@
-FROM nginx:1.26.2
+FROM nginx:1.30-alpine
 
-RUN rm /usr/share/nginx/html/*.html
+USER root
+WORKDIR /usr/share/nginx/html
 
-COPY autoinstall.yaml /usr/share/nginx/html/autoinstall.yaml
-COPY ks.cfg /usr/share/nginx/html/ks.cfg
-COPY fedora-dump.sh /usr/share/nginx/html/fedora-dump.sh
+RUN rm ./*.html
+
 COPY nginx.conf /etc/nginx/conf.d/default.conf
-COPY background.png /usr/share/nginx/html/background.png
-COPY grub /usr/share/nginx/html/grub
-COPY 00-background /usr/share/nginx/html/00-background
-COPY dump-doc.pdf /usr/share/nginx/html/dump-doc.pdf
-COPY dump.sh /usr/share/nginx/html/dump.sh
+
+COPY ./autoinstall.yaml ./autoinstall.yaml
+COPY ./background.png ./background.png
+COPY ./grub ./grub
+COPY ./00-background ./00-background
+COPY ./dump-doc.pdf ./dump-doc.pdf
+COPY ./dump.sh ./dump.sh
+COPY ./stumper/dump.sh ./stumper/dump.sh
+COPY ./stumper/alpine-answers ./stumper/alpine-answers
 
 EXPOSE 80/tcp
 
--- stumper/dump.sh
+++ stumper/dump.sh
@@ -2,10 +2,12 @@
 set -e
 
 DISK=$1
-MOUNT_POINT="/mnt"
+CHROOT="/mnt"
 PACKAGES_LIST="
   sudo
   build-base 
+  epiclang
+  banana-coding-style-checker
   emacs
   vim
   bash
@@ -22,42 +24,38 @@ PACKAGES_LIST="
   github-cli"
 
 run_cmd() {
-  chroot $MOUNT_POINT $1
+  chroot $CHROOT $1
 }
 
 ERASE_DISKS=/dev/$1 setup-alpine -e -f http://10.50.50.4:8080/stumper/alpine-answers
 
-mount /dev/$13 $MOUNT_POINT
+mount /dev/$13 $CHROOT
 
-sed -i 's/#/''/g' $MOUNT_POINT/etc/apk/repositories
+sed -i 's/#/''/g' $CHROOT/etc/apk/repositories
+echo -e "https://alpine-repo.epistone.fr" >> $CHROOT/etc/apk/repositories
+wget -O $CHROOT/etc/apk/keys/stumper-6a3e78e7.rsa.pub https://alpine-repo.epistone.fr/keys/stumper-6a3e78e7.rsa.pub
 run_cmd "apk update"
 for package in "${PACKAGES_LIST}"; do
     run_cmd "apk add $package"
 done
 
-wget -O $MOUNT_POINT/usr/local/bin/epiclang https://raw.githubusercontent.com/TheodoreGozard/epitech-dump/refs/heads/main/stumper/epiclang
-chmod +x $MOUNT_POINT/usr/local/bin/epiclang
-mkdir -p $MOUNT_POINT/usr/local/lib/epiclang/plugins
-
-wget -O $MOUNT_POINT/usr/local/lib/epiclang/plugins/epiclang-plugin-banana.so https://raw.githubusercontent.com/TheodoreGozard/epitech-dump/refs/heads/main/stumper/banana/epiclang-plugin-banana.so
-wget -O $MOUNT_POINT/usr/local/bin/banana-check-repo https://raw.githubusercontent.com/TheodoreGozard/epitech-dump/refs/heads/main/stumper/banana/banana-check-repo
-chmod +x $MOUNT_POINT/usr/local/bin/banana-check-repo
-
-wget -O $MOUNT_POINT/tmp/criterion-2.4.2.tar.xz https://raw.githubusercontent.com/TheodoreGozard/epitech-dump/refs/heads/main/stumper/criterion-2.4.2.tar.xz
-mkdir $MOUNT_POINT/tmp/criterion
-tar xf $MOUNT_POINT/tmp/criterion-2.4.2.tar.xz -C $MOUNT_POINT/tmp/criterion
-cp -r $MOUNT_POINT/tmp/criterion/libcriterion* $MOUNT_POINT/usr/local/lib
-mkdir $MOUNT_POINT/usr/local/include
-cp -r $MOUNT_POINT/tmp/criterion/include $MOUNT_POINT/usr/local/include/criterion
-mkdir $MOUNT_POINT/etc/ld.so.conf.d
-echo "/usr/local/lib" > $MOUNT_POINT/etc/ld.so.conf.d/usr-local.conf
+wget -O $CHROOT/tmp/criterion-2.4.2.tar.xz https://raw.githubusercontent.com/TheodoreGozard/epitech-dump/refs/heads/main/stumper/criterion-2.4.2.tar.xz
+mkdir $CHROOT/tmp/criterion
+tar xf $CHROOT/tmp/criterion-2.4.2.tar.xz -C $CHROOT/tmp/criterion
+cp -r $CHROOT/tmp/criterion/libcriterion* $CHROOT/usr/local/lib
+mkdir $CHROOT/usr/local/include
+cp -r $CHROOT/tmp/criterion/include $CHROOT/usr/local/include/criterion
+mkdir $CHROOT/etc/ld.so.conf.d
+echo "/usr/local/lib" >> $CHROOT/etc/ld.so.conf.d/usr-local.conf
 run_cmd "ldconfig"
-rm -rf $MOUNT_POINT/tmp/criterion-2.4.2.tar.xz $MOUNT_POINT/tmp/criterion
+rm -rf $CHROOT/tmp/criterion-2.4.2.tar.xz $CHROOT/tmp/criterion
 
-echo "stumper ALL= NOPASSWD: /sbin/reboot, /sbin/poweroff" > $MOUNT_POINT/etc/sudoers
+echo "stumper ALL= NOPASSWD: /sbin/reboot, /sbin/poweroff, /sbin/apk update" >> $CHROOT/etc/sudoers
 
-echo "rm -rf /home/stumper/!(.*)" > $MOUNT_POINT/etc/local.d/reset-stumper-home.start
-chmod +x $MOUNT_POINT/etc/local.d/reset-stumper-home.start
+echo "rm -rf /home/stumper/!(.*)" > $CHROOT/etc/local.d/reset-stumper-home.start
+chmod +x $CHROOT/etc/local.d/reset-stumper-home.start
+echo "apk update" > $CHROOT/etc/local.d/update-apk.start
+chmod +x $CHROOT/etc/local.d/update-apk.start
 run_cmd "rc-update add local default"
 
 umount /mnt
