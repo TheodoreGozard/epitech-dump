0fe1670a28ca5273dfd13efd74ff4744e15a288f
Extauren <extauren@protonmail.com>
2026-07-22 17:36:33 +0200

    add(stumper): setup git config email and name base on username

--- stumper/setup-ssh-key.sh
+++ stumper/setup-ssh-key.sh
@@ -6,3 +6,6 @@ ssh-keygen -t ed25519 -b 4096 -f /home/$USER/.ssh/id_ed25519 -N "" -C "$USER@git
 
 gh auth login -p "ssh" -s "admin:public_key" --skip-ssh-key --web
 gh ssh-key add "/home/$USER/.ssh/id_ed25519.pub" --title "stumper"
+
+git config --global user.email "$USER@epitech.eu"
+git config --global user.name "$USER"
