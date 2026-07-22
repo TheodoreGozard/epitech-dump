c72b7b4ca9b5d50e8eedb4a62afdf9a7b4ef31b9
Extauren <extauren@protonmail.com>
2026-07-22 17:00:47 +0200

    add(sumper): script to upload ssh-key on github

---
+++ stumper/setup-ssh-key.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+mkdir -p /home/$USER/.ssh
+ssh-keygen -t ed25519 -b 4096 -f /home/$USER/.ssh/id_ed25519 -N "" -C "$USER@github"
+
+gh auth login -p "ssh" -s "admin:public_key" --skip-ssh-key --web
+gh ssh-key add "/home/$USER/.ssh/id_ed25519.pub" --title "stumper"
